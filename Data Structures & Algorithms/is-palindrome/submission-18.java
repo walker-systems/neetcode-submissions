82f0e0793f80c9781287d69630cb5f21a4d2ea88
Justin Walker <justinwalker.contact@gmail.com>
2026-07-13 07:42:53 -0700

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0; 
+        int j = s.length() - 1; 
+        while (i < j && i < s.length() && j >= 0) {
+            while (!Character.isLetterOrDigit(Character.toLowerCase(s.charAt(i)))
+                && i < j
+                && i < s.length()) {
+                i++; 
+            }
+            while (!Character.isLetterOrDigit(Character.toLowerCase(s.charAt(j)))
+                && i < j
+                && j >= 0) {
+                j--; 
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    return false;
+            }
+            i++; 
+            j--; 
+        }
+        return true;
+    }
+}
